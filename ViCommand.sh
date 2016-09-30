9058c929efbeddc2823687c66f56b329236d4235
xutao <xutao@fruitday.com>
2016-09-30 17:23:23 +0800

    commit ViCommand.sh

---
+++ ViCommand.sh
@@ -0,0 +1,32 @@
+a:  insert right 
+i:  insert left
+A:  insert at the end of the line
+I:  insert at the head of the line
+o:  insert at next line 
+O:  insert at the former line
+R:  overwrite
+J:  combine the line and the next line
+
+yy: copy line
+nyy: copy n line
+yw: copy word
+nyw: copy n words
+y^: copy to line head
+y$: copy to line end
+p:  copy after
+P:  copy before
+
+u:  undo
+U:  undo all the operation of the current line
+
+:w  save
+:q  quit
+:q! quit without save
+:w! save violent
+:n  jump to line n
+:n+ jump down n line
+:n- jump up n line
+:s/old/new    replace first in line
+:s/old/new/g   replace all in line
+:n,m s/old/new/g  replace line n to m 
+:%s/old/new/g     replace all
